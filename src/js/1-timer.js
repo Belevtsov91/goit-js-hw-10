3c3fdbd53a0206036fc0905603b9ccf71ac99f29
Vitalii_Belevtsov <vitaliybelevcov@gmail.com>
2024-11-15 00:27:18 +0200

    test4

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,6 +3,8 @@
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 
 const startButton = document.querySelector('[data-start]');
